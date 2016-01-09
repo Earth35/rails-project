499b41587d9d387e5cc4388b12b6f3560ba09d40
Krzysztof Piotrowski <krzysiekp91@gmail.com>
2016-01-09 22:33:19 +0100

    Added tagging feature (Step 13.)

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  resources :tags
   root 'articles#index'
+  get 'tags/' => 'tags#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
